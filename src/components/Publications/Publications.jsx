8251d749394304d46420ef618fff4ee5d4ad46f8
Noelaxe <noelsjv@gmail.com>
2025-08-17 17:49:50 +0530

    journal articles mobile optimization

--- src/components/Publications/Publications.jsx
+++ src/components/Publications/Publications.jsx
@@ -15,7 +15,7 @@ function Publications({ filter = "ALL" }) {
     <div className="publications">
       <img src="https://static.wixstatic.com/media/ad420a_f98d68876fc145f595d999cec41dda12~mv2.jpg/v1/fill/w_1699,h_539,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ad420a_f98d68876fc145f595d999cec41dda12~mv2.jpg" />
       <div className="title">
-        <h1>Publications</h1>
+        <h1>Journal Articles</h1>
       </div>
       <div className="list">
         {isSubmenu2 ? (
--- src/components/Publications/Submenu2.jsx
+++ src/components/Publications/Submenu2.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Submenu2() {
   return (
     <div>
-      <h2>Submenu 2</h2>
+      <h2>Book Chapters</h2>
       <p>Coming soon: Content for submenu 2.</p>
     </div>
   );
