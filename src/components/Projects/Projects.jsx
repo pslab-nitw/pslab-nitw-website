0778c2d09a0c6987912a2d173ae666ec1c92a548
Noelaxe <noelsjv@gmail.com>
2025-09-06 13:48:08 +0530

    style change and projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ function Projects() {
     <div className="project">
       <div className="project-page-title">
         <img src="https://static.wixstatic.com/media/ad420a_f98d68876fc145f595d999cec41dda12~mv2.jpg/v1/fill/w_1699,h_539,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ad420a_f98d68876fc145f595d999cec41dda12~mv2.jpg"></img>
-        <h1>Project</h1>
+        <h1>Projects</h1>
       </div>
       <div className="project-container">
         {List.map((item, index) => (
